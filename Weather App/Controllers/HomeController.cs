ae42c3789734cb2aa90c1542f1c201b821c02722
Stefan Seul <65729791+GeraltofPython@users.noreply.github.com>
2020-07-22 23:15:54 -0700

    Update on Weather App

--- Weather App/Controllers/HomeController.cs
+++ Weather App/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,18 +17,72 @@ namespace Weather_App.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public JsonResult GetWeather(string wid)
         {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
+            string json = SendRequest("https://www.metaweather.com/api/location/" + wid);
+            JObject data = JObject.Parse(json);
+
+            WeatherList current = new WeatherList();
+
+            current.cityName = (string)data["title"];
+            current.locationType = (string)data["location_type"];
+
+            current.list = new List<WeatherInfo>();
+
+            for(int i = 0; i < data["consolidated_weather"].Count(); i++)
+            {
+                WeatherInfo temp = new WeatherInfo();
+
+                temp.weatherStateName = (string)data["consolidated_weather"][i]["weather_state_name"];
+                temp.minTemp = (string)data["consolidated_weather"][i]["min_temp"];
+                temp.maxTemp = (string)data["consolidated_weather"][i]["max_temp"];
+                temp.date = (string)data["consolidated_weather"][i]["applicable_date"];
+
+                current.list.Add(temp);
+            }
+
+            return Json(current, JsonRequestBehavior.AllowGet);
         }
-
-        public ActionResult Contact()
+        private string SendRequest(string uri)
         {
-            ViewBag.Message = "Your contact page.";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            /*            request.Headers.Add("Authorization", "token " + credentials);
+                        request.UserAgent = username;       // Required, see: https://developer.github.com/v3/#user-agent-required
+                        request.Accept = "application/json";*/
 
-            return View();
+            string jsonString = null;
+            // TODO: You should handle exceptions here
+            using (WebResponse response = request.GetResponse())
+            {
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                jsonString = reader.ReadToEnd();
+                reader.Close();
+                stream.Close();
+            }
+            return jsonString;
         }
     }
+
+    public class WeatherInfo
+    {
+        public string weatherStateName;
+        public string minTemp;
+        public string maxTemp;
+        public string date;
+    }
+
+    public class WeatherList
+    {
+        public string cityName;
+        public string locationType;
+        public List<WeatherInfo> list;
+    }
 }
+
+
+/* 
+ * We are going to grab wind_direction_compass, the_temp, humidity, visibility, sun_rise, sun_set, timezone
+ * add another city
+ */
---
+++ Weather App/Scripts/main.js
@@ -0,0 +1,59 @@
+﻿$(document).ready(function () {
+    $("#but").click(function () {
+        //console.log("Your but was touched.");
+        let x = $("#city-list").val();
+        console.log(x);
+
+        $.ajax({
+            type: "GET",
+            datatype: "json",
+            url: "/Home/GetWeather",
+            data: { wid: x },
+            success: ViewData,
+            error: ErrorAJAX
+        });
+    });
+});
+
+function ViewData(data) {
+    // console.log(data);
+    $("#weathers").empty();
+
+    let name = document.createElement("h1");
+    name.append(data.cityName)
+    name.className = "text-center";
+
+    $("#weathers").append(name);
+
+    for (var i = 0; i < data.list.length; i++) {
+        
+        let date = document.createElement("h3");
+        date.append(data["list"][i]["date"]);
+        date.className = "text-center";
+
+        let weather = document.createElement("p");
+        weather.append(data["list"][i]["weatherStateName"]);
+        weather.className = "text-center";
+
+        let min = document.createElement("p");
+        min.append(data["list"][i]["minTemp"]);
+        min.className = "text-center";
+
+        let max = document.createElement("p");
+        max.append(data["list"][i]["maxTemp"]);
+        max.className = "text-center";
+
+        let brick = document.createElement("br");
+
+        $("#weathers").append(date);
+        $("#weathers").append(weather);
+        $("#weathers").append(min);
+        $("#weathers").append(max);
+        $("#weathers").append(brick);
+        $("#weathers").append(brick);
+    }
+}
+
+function ErrorAJAX() {
+
+}
